470747cf1402b0da10cab0c2be7dfe57c845d9b5
prince520-lsy <114370212+prince520-lsy@users.noreply.github.com>
2024-01-12 14:26:09 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angularStackblit
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/prince520-lsy/angularStackblit)
---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,26 @@
+import { Component ,OnInit} from '@angular/core';
+
+import {ActivatedRoute} from "@angular/router"
+
+import {Product,products} from '../products'
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent {
+  product:Product|undefined
+
+  constructor(private route:ActivatedRoute){
+    
+  }
+
+  ngOnInit() {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+  
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
+  }
+}
